32cd5c8bc39ee5fd2f03e3c9e65bedcf921db3ba
Nulo <hi@nulo.in>
2025-01-03 13:39:23 -0300

    only run sentry in prod

--- sitio-sveltekit/src/hooks.server.ts
+++ sitio-sveltekit/src/hooks.server.ts
@@ -1,15 +1,15 @@
-import { sequence } from "@sveltejs/kit/hooks";
-import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
+import { sequence } from '@sveltejs/kit/hooks';
+import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit';
 import * as Sentry from '@sentry/sveltekit';
+if (import.meta.env.PROD)
+	Sentry.init({
+		dsn: 'https://2b0f8d068605d79a5cdb9e3f5ac0af06@o4508549255266304.ingest.us.sentry.io/4508570755465216',
 
-Sentry.init({
-  dsn: 'https://2b0f8d068605d79a5cdb9e3f5ac0af06@o4508549255266304.ingest.us.sentry.io/4508570755465216',
+		tracesSampleRate: 1.0
 
-  tracesSampleRate: 1.0,
-
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: import.meta.env.DEV,
-});
+		// uncomment the line below to enable Spotlight (https://spotlightjs.com)
+		// spotlight: import.meta.env.DEV,
+	});
 
 // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
 export const handle = sequence(sentryHandle());
