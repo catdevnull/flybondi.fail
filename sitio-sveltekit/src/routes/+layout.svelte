ed123322ac2383513bb4f81c48954018409d5714
Nulo <hi@nulo.in>
2025-01-03 21:04:51 -0300

    meta tags

--- sitio-sveltekit/src/routes/+layout.svelte
+++ sitio-sveltekit/src/routes/+layout.svelte
@@ -1,7 +1,40 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+
+	import cardPath from '$lib/assets/twitter-card.png';
+
+	const title = 'flybondi.fail';
+	const description =
+		'Estadísticas de demoras en vuelos de Flybondi. Mirá cuánto tiempo perdieron los pasajeros.';
+	const url = 'https://flybondi.fail';
 </script>
+
+<svelte:head>
+	{#if import.meta.env.PROD}
+		<script
+			defer
+			src="https://umami.experimentos.nulo.ar/script.js"
+			data-website-id="e919c6b5-169d-4679-bfcc-f28de2da1b44"
+		></script>
+	{/if}
+
+	<title>{title}</title>
+	<meta name="canonical" content={url} />
+	<meta name="description" content={description} />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<meta property="og:url" content={url} />
+
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@esoesnulo" />
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={description} />
+	<meta name="twitter:url" content={url} />
+	<!-- twitter requiere url completa https://leimao.github.io/blog/Missing-Image-Twitter-Card/ -->
+	<meta name="twitter:image" content={'https://flybondi.fail' + cardPath} />
+</svelte:head>
 
 <div class="min-h-screen bg-white p-4 dark:bg-neutral-900 dark:text-neutral-200">
 	{@render children()}
