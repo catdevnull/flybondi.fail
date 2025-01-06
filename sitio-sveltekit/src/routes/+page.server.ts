f5b47f94f2c0c9a5aecdeff3430a4998c3678d99
Nulo <hi@nulo.in>
2025-01-06 12:09:01 -0300

    fix dates in server with diff timezones

--- sitio-sveltekit/src/routes/+page.server.ts
+++ sitio-sveltekit/src/routes/+page.server.ts
@@ -22,9 +22,9 @@ export const load: PageServerLoad = async ({ url, platform }) => {
 	const vuelos = await sql<Vuelo[]>`
     WITH flight_data AS (
       SELECT *,
-        (to_timestamp(json->>'stda' || ' ' || to_char(last_updated, 'YYYY'), 'DD/MM HH24:MI YYYY') AT TIME ZONE 'UTC' AT TIME ZONE 'America/Buenos_Aires') AS stda,
+        (to_timestamp(json->>'stda' || ' ' || to_char(last_updated, 'YYYY'), 'DD/MM HH24:MI YYYY') AT TIME ZONE 'America/Buenos_Aires') AS stda,
         CASE 
-          WHEN LENGTH(json->>'atda') > 0 THEN (to_timestamp(json->>'atda' || ' ' || to_char(last_updated, 'YYYY'), 'DD/MM HH24:MI YYYY') AT TIME ZONE 'UTC' AT TIME ZONE 'America/Buenos_Aires')
+          WHEN LENGTH(json->>'atda') > 0 THEN (to_timestamp(json->>'atda' || ' ' || to_char(last_updated, 'YYYY'), 'DD/MM HH24:MI YYYY') AT TIME ZONE 'America/Buenos_Aires')
         END AS atda
       FROM aerolineas_latest_flight_status
       left join airfleets_matriculas
